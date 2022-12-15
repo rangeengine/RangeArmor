85b8a8f369cc302560b7d47b5ca57c59fad857ed
RafaelTavares <rafaelgt34@gmail.com>
2022-12-15 19:00:47 -0300

    RangeArmor: Fix critical launcher problem

--- source/launcher.py
+++ source/launcher.py
@@ -309,7 +309,7 @@ def copyPersistentFiles(pathFrom, pathTo, filesList):
     # type: (Path, Path, list[Path]) -> None
 
     for fileFrom in filesList:
-        fileRelative = Path(fileFrom.as_posix().replace(pathFrom.as_posix(), "")[1:])
+        fileRelative = Path(fileFrom.as_posix().replace(pathFrom.as_posix(), "")[2:])
         fileTo = (pathTo / fileRelative)
         shutil.copy2(fileFrom.as_posix(), ensurePath(fileTo).as_posix())
 
